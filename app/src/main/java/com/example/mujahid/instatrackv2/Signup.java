63ace8fc4558224e1e157e8e3eaf42c3830fcaae
Mujahid Ali <magg2761@gmail.com>
2017-11-15 03:11:48 +0530

    Updating Signup

--- app/src/main/java/com/example/mujahid/instatrackv2/Signup.java
+++ app/src/main/java/com/example/mujahid/instatrackv2/Signup.java
@@ -2,12 +2,24 @@ package com.example.mujahid.instatrackv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Signup extends AppCompatActivity {
-
+public class Signup extends AppCompatActivity implements View.OnClickListener {
+    TextView textPhone;
+    Button btnNext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        textPhone=(TextView) findViewById(R.id.etPhoneSignup);
+        btnNext=(Button) findViewById(R.id.btnSignup);
+        btnNext.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String url="";
     }
 }
