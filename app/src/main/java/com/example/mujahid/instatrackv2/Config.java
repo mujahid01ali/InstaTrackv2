ea4768f914a7e7af86f0dd6f513733ecb77b0546
Mujahid Ali <magg2761@gmail.com>
2017-11-15 03:15:36 +0530

    Adding Config java File

---
+++ app/src/main/java/com/example/mujahid/instatrackv2/Config.java
@@ -0,0 +1,9 @@
+package com.example.mujahid.instatrackv2;
+
+/**
+ * Created by Mujahid on 11/15/2017.
+ */
+
+public class Config {
+    public static final String baseUrl="http://www.amualumnimeet.com/instatrack/";
+}
