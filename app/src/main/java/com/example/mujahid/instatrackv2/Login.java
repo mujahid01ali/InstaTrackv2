a4066f1b830037c721e6425fb3c1f8c4729dda4c
Mujahid Ali <magg2761@gmail.com>
2017-11-15 02:55:24 +0530

    Adding Intent for Activity Signup

--- app/src/main/java/com/example/mujahid/instatrackv2/Login.java
+++ app/src/main/java/com/example/mujahid/instatrackv2/Login.java
@@ -1,13 +1,25 @@
 package com.example.mujahid.instatrackv2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    TextView textCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        textCreate=(TextView) findViewById(R.id.textCreate);
+        textCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentCreate =new Intent(Login.this,Signup.class);
+                startActivity(intentCreate);
+            }
+        });
     }
 }
