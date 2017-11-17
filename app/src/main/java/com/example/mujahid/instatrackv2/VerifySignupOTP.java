b9a6ad8bbce6aa2984af11854e84f900aff471cd
Mujahid Ali <magg2761@gmail.com>
2017-11-18 03:15:18 +0530

    Write checkOtp method

--- app/src/main/java/com/example/mujahid/instatrackv2/VerifySignupOTP.java
+++ app/src/main/java/com/example/mujahid/instatrackv2/VerifySignupOTP.java
@@ -1,13 +1,89 @@
 package com.example.mujahid.instatrackv2;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.renderscript.Script;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class VerifySignupOTP extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
+
+public class VerifySignupOTP extends AppCompatActivity implements View.OnClickListener {
+    Button btnOtpCheckSignup;
+    EditText etOtpSignup;
+    ProgressDialog dialog;
+    String phoneNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_signup_otp);
+        Intent intent=getIntent();
+        phoneNum=intent.getStringExtra("phone");
+        Toast.makeText(VerifySignupOTP.this,phoneNum,Toast.LENGTH_LONG).show();
+        btnOtpCheckSignup = (Button) findViewById(R.id.btnSignupOtpCheck);
+        etOtpSignup = (EditText) findViewById(R.id.etOtpVerifySignup);
+        btnOtpCheckSignup.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnOtpCheckSignup) {
+            checkOtp();
+        }
+    }
+
+    private void checkOtp() {
+        String url = Config.baseUrl+"loginOtp.php";
+        if (etOtpSignup.getText().toString().equals("")) {
+            Toast.makeText(VerifySignupOTP.this, "Please Enter OTP", Toast.LENGTH_LONG).show();
+        } else {
+        dialog=ProgressDialog.show(VerifySignupOTP.this,"","Please Wait...",false,false);
+        StringRequest strRqstVerifyOTP = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.contains("success")) {
+                    dialog.dismiss();
+                    Intent intent = new Intent(VerifySignupOTP.this, Login.class);
+                    startActivity(intent);
+                } else if (response.contains("fail")) {
+                    dialog.dismiss();
+                    Toast.makeText(VerifySignupOTP.this, "You have entered wrong number ", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                dialog.dismiss();
+
+                Toast.makeText(VerifySignupOTP.this, "Internet Connection Error", Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> param = new HashMap<>();
+                param.put("otp", etOtpSignup.getText().toString().trim());
+                param.put("phone",phoneNum.trim());
+                return param;
+            }
+        };
+        RequestHandler.getInstance(VerifySignupOTP.this).addToRequestQueue(strRqstVerifyOTP);
+    }
+    }
+
 }
