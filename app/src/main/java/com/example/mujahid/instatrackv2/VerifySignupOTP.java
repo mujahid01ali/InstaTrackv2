52743c52347ec01a74d7055adf69de0513ad55d4
Naushad Alam <ngc9458@gmail.com>
2017-11-18 01:59:45 +0530

    Adding Signup OTP verification activity to project

---
+++ app/src/main/java/com/example/mujahid/instatrackv2/VerifySignupOTP.java
@@ -0,0 +1,13 @@
+package com.example.mujahid.instatrackv2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerifySignupOTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_signup_otp);
+    }
+}
