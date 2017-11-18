28b754e8231e74286061b7321679ba289dbdbd98
Naushad Alam <ngc9458@gmail.com>
2017-11-18 16:40:20 +0530

    Adding code for volley retry policy

--- app/src/main/java/com/example/mujahid/instatrackv2/Signup.java
+++ app/src/main/java/com/example/mujahid/instatrackv2/Signup.java
@@ -1,17 +1,16 @@
 package com.example.mujahid.instatrackv2;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -71,6 +70,8 @@ public class Signup extends AppCompatActivity implements View.OnClickListener {
                     return param;
                 }
             };
+            strRqstSignup.setRetryPolicy(new DefaultRetryPolicy(30000,DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
             RequestHandler.getInstance(Signup.this).addToRequestQueue(strRqstSignup);
         }
     }
