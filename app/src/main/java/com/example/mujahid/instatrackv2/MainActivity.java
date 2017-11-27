4ea69cb616a6e33ce38264faf5a34a499febaa5b
Mujahid Ali <magg2761@gmail.com>
2017-11-27 23:19:21 +0530

    Adding Create Group Button

--- app/src/main/java/com/example/mujahid/instatrackv2/MainActivity.java
+++ app/src/main/java/com/example/mujahid/instatrackv2/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.mujahid.instatrackv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnGroupCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnGroupCreate=(Button) findViewById(R.id.btnCreateGroup);
+        btnGroupCreate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        
     }
 }
