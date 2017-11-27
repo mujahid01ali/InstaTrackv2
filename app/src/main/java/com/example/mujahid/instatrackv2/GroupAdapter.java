122fa68061f3beb844514c253e6cad513a7c3325
Mujahid Ali <magg2761@gmail.com>
2017-11-28 00:49:13 +0530

    Adding GroupAdapter.Java file for RecyclerView

--- app/src/main/java/com/example/mujahid/instatrackv2/GroupAdapter.java
+++ app/src/main/java/com/example/mujahid/instatrackv2/GroupAdapter.java
@@ -1,8 +1,56 @@
 package com.example.mujahid.instatrackv2;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import java.util.List;
 /**
  * Created by Mujahid on 11/28/2017.
  */
 
-public class GroupAdapter {
+public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.MyViewHolder> {
+
+    private Context mCtx;
+    private List<Group> groupList;
+
+    public GroupAdapter(Context mCtx,List<Group> groupList){
+        this.mCtx=mCtx;
+        this.groupList=groupList;
+    }
+
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view=inflater.inflate(R.layout.group_list_row,null);
+        return new MyViewHolder(view);
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Group group=groupList.get(position);
+        holder.name.setText(group.getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return groupList.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView name;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            name=(TextView) itemView.findViewById(R.id.name);
+
+            }
+    }
 }
