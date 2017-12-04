5098e21bff6f98c83ab6e3ed73c4fc282b1767e4
Naushad Alam <ngc9458@gmail.com>
2017-12-05 03:38:11 +0530

    Adding code for long press, adding long press menu xml

--- app/src/main/java/com/example/mujahid/instatrackv2/MainActivity.java
+++ app/src/main/java/com/example/mujahid/instatrackv2/MainActivity.java
@@ -2,9 +2,9 @@ package com.example.mujahid.instatrackv2;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,7 +14,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -61,6 +60,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }
                 })
         );
+
+
         getGroups();
 
         btnGroupCreate.setOnClickListener(this);
@@ -141,4 +142,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivity(intent);
 
     }
+
 }
