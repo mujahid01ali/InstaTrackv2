2d92af1e5d6c789c3492486210ebf59fd1787c41
Mujahid Ali <magg2761@gmail.com>
2017-12-05 03:07:21 +0530

    Chaging intent value

--- app/src/main/java/com/example/mujahid/instatrackv2/MainActivity.java
+++ app/src/main/java/com/example/mujahid/instatrackv2/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 new RecyclerItemClickListener(this, new RecyclerItemClickListener.OnItemClickListener() {
                     @Override public void onItemClick(View view, int position) {
                         Group group = (Group) groupList.get(position);
-                        Intent intent=new Intent(MainActivity.this,GroupActivity.class);
+                        Intent intent=new Intent(MainActivity.this,GroupUsers.class);
                         intent.putExtra("groupId",group.getId().trim());
                         startActivity(intent);
                        // Toast.makeText(getApplicationContext(),"group Name is "+ group.getName()+" and Group id is "+group.getId() + " is selected!", Toast.LENGTH_SHORT).show();
